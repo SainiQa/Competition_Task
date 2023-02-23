359640791b5575eaa70367eb42ec6cc4c4ed0caf
unknown <official.psaini31@gmail.com>
2023-02-23 23:50:54 +1300

    Framework

---
+++ Competition_Task/Competition_Task/Tests/ShareSkill_Tests.cs
@@ -0,0 +1,16 @@
+namespace Competition_Task.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Competition_Task/Competition_Task/Utilities/Common.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competition_Task.Utilities
+{
+    public class Common
+    {
+        public static IWebDriver driver;
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public static void ImpliWait()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        }
+
+        public void Close()
+        {
+            driver.Quit();
+
+        }
+
+    }
+}
