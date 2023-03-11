503304409a1a99b8aad62ac8a3d1be9246626d1a
unknown <official.psaini31@gmail.com>
2023-03-11 21:36:44 +1300

    First Commit on Branch with all Features

--- Competition_Task/Competition_Task/Pages/ManageListingsPage.cs
+++ Competition_Task/Competition_Task/Pages/ManageListingsPage.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Competition_Task.ExcelReader;
+using Competition_Task.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,75 @@ using System.Threading.Tasks;
 
 namespace Competition_Task.Pages
 {
-    public class ManageListingsPage
+    public class ManageListingsPage:Common
     {
+        //public ManageListingsPage()
+
+        //{
+        //    PageFactory.InitElements(driver, this);
+        //}
+
+        [FindsBy(How = How.XPath, Using = "//section[@class='nav-secondary']/div[@class='ui eight item menu']/a[@href='/Home/ListingManagement']")]
+
+        public IWebElement manageListingTab;
+
+        [FindsBy(How = How.XPath, Using = "//table[@class='ui striped table']/tbody/tr[1]/td[8]/div[@class='ui small icon buttons basic vertical']/button[2]/i[@class='outline write icon']")]
+        
+        public IWebElement editIcon;
+       
+        [FindsBy(How = How.XPath, Using = "//table[@class='ui striped table']/tbody/tr[1]/td[8]/div[@class='ui small icon buttons basic vertical']/button[3]/i[@class='remove icon']")]
+        public IWebElement deleteIcon;
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[2]/div/div[3]/button[2]")]
+        public IWebElement deleteConfirm { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//table[@class='ui striped table']/tbody/tr[1]/td[3]")]
+        
+        public IWebElement titleValidate;           
+
+
+        public void NavigateToEdit()
+        {
+            PageFactory.InitElements(driver, this);
+            manageListingTab.Click();
+            Thread.Sleep(5000);
+            editIcon.Click();
+            Wait(3);           
+
+        }
+    public string GetCreateDelete()
+        {
+           PageFactory.InitElements(driver, this);
+            
+            manageListingTab.Click();
+            Thread.Sleep(2000);
+            string DeletedTitleText = titleValidate.Text;
+            return DeletedTitleText;
+        }
+
+        public void DeleteSkills()
+        {            
+            PageFactory.InitElements(driver, this);
+            manageListingTab.Click();
+            Thread.Sleep(3000);
+            deleteIcon.Click();
+            //driver.SwitchTo().Alert().Accept();
+            deleteConfirm.Click();
+            Wait(3);               
+
+
+        }
+        public string DeleteTitletocompare()
+        {
+            PageFactory.InitElements(driver, this);
+            ExcelUtil.PopulateInCollection(@"D:\Competition_Task\Competition_Task\Competition_Task\Competition_Task\Data\SkillShare.xlsx", "ShareSkill");
+
+            string TitlefromExcel = ExcelUtil.ReadData(3, "Title");
+            return TitlefromExcel;
+        }
+
     }
-}
+    }
+
+
+
--- Competition_Task/Competition_Task/Utilities/Common.cs
+++ Competition_Task/Competition_Task/Utilities/Common.cs
@@ -5,25 +5,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Competition_Task.ExcelReader;
+using Competition_Task.Pages;
+using NUnit.Framework;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 
 namespace Competition_Task.Utilities
 {
     public class Common
-    {
+    {     
         public static IWebDriver driver;
-        public void Initialize()
+        //LoginPage loginPageObj;
+         SignupPage signupPageObj;
+
+        public static ExtentReports extentReportObj = null;
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentTest test;
+        static string reportPath = System.IO.Directory.GetParent(@"../../../").FullName +
+        Path.DirectorySeparatorChar + "ExtentReports" +
+        Path.DirectorySeparatorChar + "Result " + DateTime.Now.ToString("ddMMyyyy HHmmss");
+
+        [OneTimeSetUp]
+        public void LoginActions()
         {
+
+            //htmlReporter = new ExtentHtmlReporter(@"D:\Competition_Task\Competition_Task\Competition_Task\Competition_Task\ExtentReports\SkillShare.html");
+
+            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(reportPath);
+            extentReportObj = new ExtentReports();
+            extentReportObj.AttachReporter(htmlReporter);
+
             driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5000");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(5000);
+            LoginPage loginPageObj = new LoginPage();
+            loginPageObj.LoginSteps();
+
+            
         }
 
-        public static void ImpliWait()
+        public static void Wait(int second)
         {
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(second);
         }
 
+
+        public class GetScreenShot
+        {
+            public static string Capture(IWebDriver driver, string screenShotName)
+            {
+                ITakesScreenshot ts = (ITakesScreenshot)driver;
+                Screenshot screenshot = ts.GetScreenshot();
+                string pth = System.Reflection.Assembly.GetCallingAssembly().Location;
+                string finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "Screenshots\\" + screenShotName + ".png";
+                string localpath = new Uri(finalpth).LocalPath;
+                screenshot.SaveAsFile(localpath, ScreenshotImageFormat.Png);
+                return localpath;
+            }
+        }
+
+
+
+        [OneTimeTearDown]
         public void Close()
-        {
+        {           
+
+            extentReportObj.Flush();
             driver.Quit();
 
         }
